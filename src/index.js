0bf21665148fed952ff3d4ecbc07651036f9afbd
Sergey Orekhov <sergejorehov1992@gmail.com>
2023-02-12 12:09:46 +0300

    fix: mistakes

--- src/index.js
+++ src/index.js
@@ -1,26 +1,18 @@
-module.exports = function check(str, bracketsConfig) {
-  const arrLeft = [];
-  const arrRight = [];
-  let arr = str.split("");
+module.exports = function check1(str, bracketsConfig) {
+  const stack = [];
+  const config = {};
+  let current;
   for (let i = 0; i < bracketsConfig.length; i++) {
-    arrLeft.push(bracketsConfig[i][0]);
-    arrRight.push(bracketsConfig[i][1]);
+    config[bracketsConfig[i][0]] = bracketsConfig[i][1];
   }
-  const fun = (arr) => {
-    for (let j = 1; j < arr.length; j++) {
-      let last = arr[j - 1];
-      let indexLast = arrLeft.indexOf(last);
-      let curent = arr[j];
-      let indexCurent = arrRight.indexOf(curent);
-      if (!(arrLeft.includes(curent) && indexLast !== indexCurent)) {
-        if (!(indexCurent == indexLast)) return false;
-        if (arrRight.includes(last) && curent !== last) return false;
-        arr.splice([j - 1], 2);
-        if (arr.length == 0) return true;
-        return fun(arr);
-      }
+  for (let j = 0; j < str.length; j++) {
+    current = str[j];
+    let index = stack[stack.length - 1];
+    if (config[index] == current) {
+      stack.pop();
+    } else {
+      stack.push(current);
     }
-    return false;
-  };
-  return fun(arr);
+  }
+  return stack.length === 0;
 };
