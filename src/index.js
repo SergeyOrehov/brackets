ed41a0a7f57882caab731fe1776609cdfa15d981
Sergey Orekhov <sergejorehov1992@gmail.com>
2023-02-12 12:12:43 +0300

    fix: mistakes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 module.exports = function check1(str, bracketsConfig) {
   const stack = [];
   const config = {};
-  let current;
   for (let i = 0; i < bracketsConfig.length; i++) {
     config[bracketsConfig[i][0]] = bracketsConfig[i][1];
   }
   for (let j = 0; j < str.length; j++) {
-    current = str[j];
-    let index = stack[stack.length - 1];
-    if (config[index] == current) {
+    if (config[stack[stack.length - 1]] == str[j]) {
       stack.pop();
     } else {
-      stack.push(current);
+      stack.push(str[j]);
     }
   }
   return stack.length === 0;
